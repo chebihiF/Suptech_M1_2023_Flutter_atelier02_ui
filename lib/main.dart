6644098000f9ecba73a8118072a37431cd552017
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-06-08 20:55:04 +0100

    add first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:atelier02_ui/screens/intro_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,32 +11,7 @@ class GlobeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: const Text('Globo Fitness')),
-            body: Container(
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/fitness.jpeg'),
-                      fit: BoxFit.cover)),
-              child: Center(
-                child: Container(
-                  padding: const EdgeInsets.all(24),
-                  margin: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: const Text(
-                    'Commit to bi fit, dare to be greate with Globo Fitness',
-                    style: TextStyle(fontSize: 20, shadows: [
-                      Shadow(
-                          offset: Offset(1.0, 1.0),
-                          blurRadius: 2.0,
-                          color: Colors.grey)
-                    ]),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            )));
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const IntroScreen());
   }
 }
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Globo Fitness')),
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/fitness.jpeg'), fit: BoxFit.cover)),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              margin: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: const Text(
+                'Commit to be fit, dare to be great with Globo Fitness',
+                style: TextStyle(fontSize: 20, shadows: [
+                  Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 2.0,
+                      color: Colors.grey)
+                ]),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ));
+  }
+}
